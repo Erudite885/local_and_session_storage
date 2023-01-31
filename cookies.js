b11509c37c0ac243fb2ded34222e07e641358a4b
Erudite885 <erudite885@gmail.com>
2023-01-31 12:35:37 +0100

    renamed app.js to cookies.js

---
+++ cookies.js
@@ -0,0 +1,20 @@
+const storeBtn = document.getElementById("store-btn");
+const retrBtn = document.getElementById("retrieve-btn");
+
+storeBtn.addEventListener("click", () => {
+  const userId = "u123";
+  const user = {
+    name: "matt",
+    age: 25,
+  };
+  document.cookie = `uid=${userId}`;
+  document.cookie = `uid=${JSON.stringify(user)}`;
+});
+
+retrBtn.addEventListener("click", () => {
+  const cookieData = document.cookie.split(";");
+  const data = cookieData.map((i) => {
+    return i.trim();
+  });
+  console.log(data);
+});
