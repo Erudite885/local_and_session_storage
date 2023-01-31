06a24ce5a1e16f63ffee72ffe064c0fc18524caf
Erudite885 <erudite885@gmail.com>
2023-01-31 12:32:13 +0100

    working with cookies

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const retrBtn = document.getElementById("retr-btn");
 
 storeBtn.addEventListener("click", () => {
   const userId = "u123";
+  const user = {
+    name: "matt",
+    age: 25,
+  };
   document.cookie = `uid=${userId}`;
+  document.cookie = `uid=${JSON.stringify(user)}`;
 });
+
 retrBtn.addEventListener("click", () => {
-  console.log(document.cookie);
+  const cookieData = document.cookie.split(";");
+  const data = cookieData.map((i) => {
+    return i.trim();
+  });
+  console.log(data);
 });
