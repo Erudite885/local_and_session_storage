9b51ac79ffc42853459fe766f3ec5f14bd4807df
Erudite885 <erudite885@gmail.com>
2023-01-31 13:12:34 +0100

    working with indexedDB

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const storeBtn = document.getElementById("store-btn");
 const retrBtn = document.getElementById("retrieve-btn");
 
+let db;
+
 const dbRequest = indexedDB.open("storageDummy", 1);
+
+dbRequest.onsuccess = function (event) {
+  db = event.target.result;
+};
+
 dbRequest.onupgradeneeded = function (event) {
-  const db = event.target.result;
+  db = event.target.result;
 
   const objStore = db.createObjectStore("products", { keyPath: "id" });
 
@@ -24,6 +31,28 @@ dbRequest.onerror = function (event) {
   console.log("error!!!!!!!!!!!!!!");
 };
 
-storeBtn.addEventListener("click", () => {});
+storeBtn.addEventListener("click", () => {
+  if (!db) {
+    return;
+  }
+  const productsStore = db
+    .transaction("products", "readonly")
+    .objectStore("products");
+  productsStore.add({
+    id: "p2",
+    title: "A Second Product",
+    price: 100,
+    tags: ["pricy", "posh", "luxury"],
+  });
+});
 
-retrBtn.addEventListener("click", () => {});
+retrBtn.addEventListener("click", () => {
+  const productsStore = db
+    .transaction("products", "readonly")
+    .objectStore("products");
+  const request = productsStore.get("p2");
+
+  request.onsuccess = function () {
+    console.log(request.result);
+  };
+});
