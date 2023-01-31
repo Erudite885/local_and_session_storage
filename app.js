17109a962cd1fa37cab14592d1e80a8ed6574845
Erudite885 <erudite885@gmail.com>
2023-01-31 12:38:35 +0100

    indexdb

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
 const storeBtn = document.getElementById("store-btn");
-const retrBtn = document.getElementById("retr-btn");
+const retrBtn = document.getElementById("retrieve-btn");
 
-storeBtn.addEventListener("click", () => {
-  const userId = "u123";
-  const user = {
-    name: "matt",
-    age: 25,
-  };
-  document.cookie = `uid=${userId}`;
-  document.cookie = `uid=${JSON.stringify(user)}`;
-});
+storeBtn.addEventListener("click", () => {});
 
-retrBtn.addEventListener("click", () => {
-  const cookieData = document.cookie.split(";");
-  const data = cookieData.map((i) => {
-    return i.trim();
-  });
-  console.log(data);
-});
+retrBtn.addEventListener("click", () => {});
--- indexedDB.js
+++
@@ -1,6 +0,0 @@
-const storeBtn = document.getElementById("store-btn");
-const retrBtn = document.getElementById("retrieve-btn");
-
-storeBtn.addEventListener("click", () => {});
-
-retrBtn.addEventListener("click", () => {});
