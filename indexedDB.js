11c2706a1eebaa117c9a7d0ca893b5be6270fd42
Erudite885 <erudite885@gmail.com>
2023-01-31 12:37:26 +0100

    getting started with indexedDB

---
+++ indexedDB.js
@@ -0,0 +1,6 @@
+const storeBtn = document.getElementById("store-btn");
+const retrBtn = document.getElementById("retrieve-btn");
+
+storeBtn.addEventListener("click", () => {});
+
+retrBtn.addEventListener("click", () => {});
